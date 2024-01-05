4c8df2dc84eb9a0e7ff402c16bc1c1bd1befec65
chessyu <472734301@qq.com>
2024-01-05 16:39:21 +0800

    feat: optimize style

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,12 +1,18 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import LayoutHeader from './LayoutHeader'
+import LayoutFooter from './LayoutFooter'
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <Box component="main"
-        
+            sx={{
+                height: '100vh'
+            }}
         >
+            <LayoutHeader />
             {children}
+            <LayoutFooter />
         </Box>
     )
 }
